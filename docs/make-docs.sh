db6688f33eb5f0124a4c7281555a4737022b8301
mjg6v <mjg6v@pop-os.localdomain>
2020-11-05 16:17:32 -0500

    adding local pandoc script

--- docs/make-docs.sh
+++ docs/make-docs.sh
@@ -6,6 +6,3 @@ for f in ../aca-framework/src/*.lhs; do
     html_f=`basename $f .lhs`.html
     pandoc $f -f markdown+lhs -t html -s -o src/$html_f
 done
-
-git add .
-git commit -m 'auto-generated documentation'
