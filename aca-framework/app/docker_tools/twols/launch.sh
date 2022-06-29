f5087492325a994164fe7f142e98c4fd300b6dfb
mjg6v <mjg6v@pop-os.localdomain>
2022-06-28 23:40:24 -0400

    correcting comment

--- aca-framework/app/docker_tools/twols/launch.sh
+++ aca-framework/app/docker_tools/twols/launch.sh
@@ -2,7 +2,7 @@
 
 # run from 2ls dir
 cd /2ls/
-# run veriabs
+# run 2ls
 OUTPUT=$(./2ls /alpaca_in/*.c --32 --graphml-witness /alpaca_out/witness.graphml --propertyfile /PropertyUnreachCall.prp)
 # report full result details
 echo $OUTPUT
