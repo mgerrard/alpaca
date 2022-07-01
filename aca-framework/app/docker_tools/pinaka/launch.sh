bf6986c1f9e6056f6b8b3ef8623da21fde6ad2ca
mjg6v <mjg6v@pop-os.localdomain>
2022-07-01 14:18:11 -0400

    pinaka works with up-to-date error function definitions

--- aca-framework/app/docker_tools/pinaka/launch.sh
+++ aca-framework/app/docker_tools/pinaka/launch.sh
@@ -3,6 +3,6 @@
 # run from pinaka dir
 cd /pinaka/
 # run pinaka
-OUTPUT=$(./pinaka-wrapper.sh --graphml-witness /alpaca_out/witness.graphml --propertyfile PropertyUnreachCall.prp --32 /alpaca_in/*.c)
+OUTPUT=$(./pinaka-wrapper.sh --graphml-witness /alpaca_out/witness.graphml --propertyfile /PropertyUnreachCall.prp --32 /alpaca_in/*.c)
 # report full result details
 echo $OUTPUT
