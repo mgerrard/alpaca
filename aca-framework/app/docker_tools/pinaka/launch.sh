376fbfb207746e29adbdc434d740f57b2c7d1e22
mjg6v <mjg6v@pop-os.localdomain>
2022-07-01 14:05:24 -0400

    adding shell of pinaka; also not yet working

---
+++ aca-framework/app/docker_tools/pinaka/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run from pinaka dir
+cd /pinaka/
+# run pinaka
+OUTPUT=$(./pinaka-wrapper.sh --graphml-witness /alpaca_out/witness.graphml --propertyfile PropertyUnreachCall.prp --32 /alpaca_in/*.c)
+# report full result details
+echo $OUTPUT
