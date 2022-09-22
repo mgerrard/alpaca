5c9a36c1d70593f76eba9517595e849b8e0dccb4
mjg6v <mjg6v@pop-os.localdomain>
2022-09-22 14:49:09 -0400

    pared down install script

--- install.sh
+++ install.sh
@@ -4,14 +4,6 @@ function check_for {
     command -v $1 >/dev/null 2>&1 || { echo >&2 "I require $1 but it's not installed.  Aborting."; exit 1; }
 }
 
-function check_for_python_3_6 {
-    command -v "python3.6" >/dev/null 2>&1 || { echo >&2 "Please create a symbolic link from python3.6 to your version of python3 (e.g., 3.8) with the command 'sudo ln -s /usr/bin/python3.8 /usr/bin/python3.6'.  Some tools in the portfolio look explicitly for python3.6, unfortunately. Aborting."; exit 1; }
-}
-
-function check_for_python {
-    command -v "python" >/dev/null 2>&1 || { echo >&2 "Please create a symbolic link from python to your version of python2 (e.g., 2.7) with the command 'sudo ln -s /usr/bin/python2.7 /usr/bin/python'.  Some tools in the portfolio have a wrapper script that uses constructs only legal in python 2, unfortunately. Aborting."; exit 1; }
-}
-
 verlte() {
     [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
@@ -40,24 +32,12 @@ function check_java_version {
     }
 }
 
-function does_package_exist {
-    dpkg -l $1 >/dev/null 2>&1 || { echo >&2 "I cannot find $1 in your package manager; please install it. Aborting."; exit 1; }
-}
-
 check_for_stack
 check_stack_version
 check_java_version
-check_for "python3"
-check_for_python_3_6
-check_for_python
 check_for "z3"
-check_for "ant"
 check_for "git"
 check_for "docker"
-does_package_exist "libc6-dev-i386"
-does_package_exist "python-pycparser"
-does_package_exist "libgmp-dev"
-does_package_exist "libtinfo-dev"
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ANALYZERS="$ROOT/tools/analyzer-portfolio/"
@@ -86,116 +66,13 @@ CIVL_DIR="$ANALYZERS/civl"
 CIVL_JAR="$CIVL_DIR/civl.jar"
 java -jar $CIVL_JAR config
 
-CPA_SE_DIR="$ANALYZERS/cpa_symexec"
-CPA_REPO_DIR="$ANALYZERS/cpa_symexec/cpachecker"
-CPA_DSE_CONFIG="$CPA_SE_DIR/violation-witness-validation-symexec.properties"
-if [ ! -d "$CPA_REPO_DIR" ]; then
-    echo
-    echo "Cloning CPA-checker repo"
-    cd $CPA_SE_DIR
-    git clone https://github.com/sosy-lab/cpachecker.git
-    cd $CPA_REPO_DIR
-    # the following is the specific version of CPA tested
-    git checkout a177be94f34033b02bc0f078fd6e24b210255aea
-    echo
-    echo "Building CPA"
-    ant
-    cp $CPA_DSE_CONFIG $CPA_REPO_DIR/config
-    cd $ROOT
-else
-    echo
-    echo "CPA-checker repo already exists; moving on."
-fi
-
-function unpack_and_rename_zip_if_needed {
-    # first: canonical folder name (e.g., CPA_Seq)
-    # second: zip file name (e.g., cpa-seq)
-    # third: unpacked folder name (e.g., CPAchecker-1.6.23-svn\ 26773-unix)
-    if [ ! -d "$1" ]; then
-	echo "-> unpacking $1"
-	if [[ $1 == "InterpChecker" ]] || [[ $1 == "VeriAbs" ]]; then
-	    unzip -q $2.zip
-        else
-	    unzip -q $2.zip && mv "$3" $1;
-	fi
-    fi
-}
-
-ARCHIVE_REPO="https://gitlab.com/sosy-lab/sv-comp/archives-2020/raw/svcomp20/2020"
-
-function download_zip_if_needed {
-
-    zip_file="$1.zip"
-    expected_size="$2"
-    unpacked_name="$3"
-    download_link="$ARCHIVE_REPO/$zip_file"
-     
-    if [ ! -f "$zip_file" ]; then
-	echo "i didnt find $zip_file in $SV_ARCHIVES"
-	echo "downloading it from $download_link"
-	wget $download_link
-    else
-	actual_size=$(stat -c %s "$zip_file")
-        if [ $actual_size -eq $expected_size ]; then
-	    echo "you have the latest $zip_file, moving on..."
-	else
-	    echo "removing stale $zip_file"
-	    rm -f $zip_file
-	    echo "removing possible stale unpacked folder"
-	    rm -rf $unpacked_name
-
-	    echo "downloading new $zip_file from $download_link"
-       	    wget $download_link	    
-	fi
-    fi
-}
-
-echo
-echo "Downloading SVCOMP archives"
-echo
-cd $SV_ARCHIVES
-
-download_zip_if_needed "cpa-seq" "102733640" "CPA_Seq"
-download_zip_if_needed "uautomizer" "84313069" "UAutomizer"
-download_zip_if_needed "symbiotic" "73653236" "Symbiotic"
-download_zip_if_needed "esbmc" "39176877" "ESBMC"
-download_zip_if_needed "pesco" "289247537" "Pesco"
-download_zip_if_needed "veriabs" "313055755" "VeriAbs"
-download_zip_if_needed "2ls" "3983396" "TwoLS"
-download_zip_if_needed "cbmc" "6832746" "CBMC"
-download_zip_if_needed "utaipan" "84306647" "UTaipan"
-download_zip_if_needed "ukojak" "84301613" "UKojak"
-download_zip_if_needed "cpa-bam-bnb" "101242097" "CPA_BAM_BnB"
-download_zip_if_needed "pinaka" "50696530" "Pinaka"
-
-echo
-echo "Unpacking any zipped SVCOMP archives"
-
-unpack_and_rename_zip_if_needed "CPA_Seq" "cpa-seq" "CPAchecker-1.9-unix"
-unpack_and_rename_zip_if_needed "UAutomizer" "uautomizer" "UAutomizer-linux"
-unpack_and_rename_zip_if_needed "Symbiotic" "symbiotic" "symbiotic"
-unpack_and_rename_zip_if_needed "ESBMC" "esbmc" "esbmc"
-unpack_and_rename_zip_if_needed "Pesco" "pesco" "PeSCo-1.8.2"
-unpack_and_rename_zip_if_needed "VeriAbs" "veriabs"
-unpack_and_rename_zip_if_needed "TwoLS" "2ls" "2ls"
-unpack_and_rename_zip_if_needed "CBMC" "cbmc" "cbmc"
-unpack_and_rename_zip_if_needed "UTaipan" "utaipan" "UTaipan-linux"
-unpack_and_rename_zip_if_needed "UKojak" "ukojak" "UKojak-linux"
-unpack_and_rename_zip_if_needed "CPA_BAM_BnB" "cpa-bam-bnb" "cpa-bam-bnb"
-unpack_and_rename_zip_if_needed "Pinaka" "pinaka" "pinaka"
-
 cd $ROOT
 echo
-echo "Now running a sanity check on each tool; this may take some time..."
-./aca-framework/test/sanity_check.sh
+echo "Set up CPA's Docker image by moving to ./aca-framework/app/docker_tools/cpa and running:"
+echo "  sudo docker build cpa ."
 echo
-echo "Please report a bug if there are any above failures."
+echo "Set up ESBMC's Docker image by moving to ./aca-framework/app/docker_tools/esbmc and running:"
+echo "  sudo docker build esbmc ."
 echo
-echo "To see options (assuming $HOME/.local/bin is on your PATH), run:"
-echo
-echo "  alpaca -h"
-echo
-echo "To test out ALPACA (with debugging on) on a single analyzer, run:"
-echo
-echo "  cd examples; alpaca -d full -p cpaSeq test.c"
+echo "Now try running `alpaca -p cpaSeq,esbmc ./examples/demo.c`"
 echo
